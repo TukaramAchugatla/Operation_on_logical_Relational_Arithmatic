52752d471af02f034307f7f0d04a6081a30f1417
TukaramAchugatla <achugatlatukaram@gmail.com>
2025-05-05 09:06:06 +0530

    Initial commit

---
+++ Default/src/lab/tka/code/Values.java
@@ -0,0 +1,13 @@
+package lab.tka.code;
+
+public class Values {
+
+	public byte b;
+	public short s;
+    public int i;
+    public long l;
+    public float f;
+    public double d;
+    public char c;
+    public boolean bol;
+}
